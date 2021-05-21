f1263d6748a9d974a3936609624e723ef2a307fb
maD-skillz <warneverchang3s@gmail.com>
2021-05-21 12:52:21 +0300

    2. Каркас шахматной доски [#331705]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.chess;
 
+import com.sun.javafx.font.coretext.CTFactory;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
@@ -9,15 +11,31 @@ import ru.job4j.chess.firuges.white.BishopWhite;
 
 public class LogicTest {
 
-    @Test
-    public void move()
+    @Test(expected = ImpossibleMoveException.class)
+    public void whenImpossibleToMove()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.C1);
-    /*    logic.add(new BishopBlack(Cell.C3));
-        logic.add(new BishopBlack(Cell.A5));
-        logic.move(Cell.C3, Cell.A5);
-        logic.add(new BishopWhite(Cell.A2)); */
+        logic.move(Cell.C1, Cell.C7);
+        logic.clean();
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void whenFigureNotFound()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.E3));
+        logic.clean();
+
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void whenOccupiedCell()
+            throws ImpossibleMoveException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.B2));
+        logic.add(new BishopBlack(Cell.F6));
+        logic.move(Cell.B2, Cell.G7);
+        logic.clean();
     }
 }
